f667524e9afa54ab3a1e9118e2d37dcd4a6a62c1
maducz <160507829+maducz@users.noreply.github.com>
2024-03-01 11:27:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function mudaTexto(){
+    document.getElementById('paragrafo1').innerHTML = "Ola, eu sou o Javascript"
+}
+
+function mostraHora(){
+  document.getElementById("paragrafo2").innerHTML = Date()
+}
